e7ce029f5dad096074416c9eeff108e5fa9f090e
standder <standder2@gmail.com>
2022-05-10 21:26:00 +0800

    update ex3.py

---
+++ ex3.py
@@ -0,0 +1,9 @@
+print("I will now count my chickens:")
+
+print("Hens",225 +30 / 6)
+print("R", 100-25*3%4)
+
+print("Now I will count the eggs:")
+
+print("Is it true that 3 + 2 < 5 - 7 ?")
+print(3+2<5-7)
