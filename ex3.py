4961af3bfcff3e72148cf2886402c1e4f86fdd28
standder <standder2@gmail.com>
2022-05-11 18:06:19 +0800

    add ex3.py

--- ex3.py
+++ ex3.py
@@ -7,3 +7,11 @@ print("Now I will count the eggs:")
 
 print("Is it true that 3 + 2 < 5 - 7 ?")
 print(3+2<5-7)
+
+print(3+2+1-5+4%2)
+
+print("what is 3 + 2",3+2)
+print("what is 5 - 7", 5-7)
+
+print("Oh, that 's why it's False.")
+print("How about some more?")
