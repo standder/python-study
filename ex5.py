fc452c0302268b1c73cdddc5ea62d1d3cdaa61ea
standder <standder2@gmail.com>
2022-05-11 21:26:09 +0800

    add ex5.py

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = "toto worf"
+my_age = 100
+my_height = 150
+my_weight = 60
+my_eyes = "black"
+my_teeth = "White"
+my_hair = "Black"
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee")
+
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age},{my_height},and {my_weight} I get {total}")
