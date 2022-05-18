c8a2bbb9746bfa645949fac7c0e331d26d2a34da
standder <standder2@gmail.com>
2022-05-18 17:04:00 +0800

    add ex6.py

---
+++ ex6.py
@@ -0,0 +1,14 @@
+types_of_people = 10
+x = f"There are {types_of_people} types of people."
+
+binary = "biinary"
+do_not = "don't"
+y = f"Theose who know {binary} and those who {do_not}."
+
+print(x)
+print(y)
+
+hilarious = False
+joke_evalution = "Isn't that joke so funny?! {}"
+
+print(joke_evalution.format(hilarious))
